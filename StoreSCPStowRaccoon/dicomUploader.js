1cd580462bb73a2aac2e0233858b1314675a0120
kevin20888802 <kevin20888802@gmail.com>
2023-03-22 21:41:09 +0800

    feat: create folder first before move

--- StoreSCPStowRaccoon/dicomUploader.js
+++ StoreSCPStowRaccoon/dicomUploader.js
@@ -4,23 +4,17 @@ const os = require('os');
 const uuid = require('uuid');
 
 const { stowFolder } = require("./stowStandalone/stowStandalone");
-
+var randomStr = "";
 const dicomDir = './dicomFiles/'; // 指定Dicom目錄
-const tempDir = path.join('./temp/', `dicom_${Math.random().toString(36).substring(2)}`); // 建立隨機產生的暫存目錄
+var tempDir = ""; // 建立隨機產生的暫存目錄
 
 // 遞歸遍歷目錄下的所有Dicom文件，將它們移動到暫存目錄
 function moveDicomFilesToTempDir(dirPath) {
   fs.readdirSync(dirPath).forEach(file => {
     const filePath = path.join(dirPath, file);
-    console.log(filePath);
     if (fs.statSync(filePath).isDirectory()) {
       moveDicomFilesToTempDir(filePath);
     } else {
-      // 創建暫存目錄，如果不存在的話
-      if (!fs.existsSync(tempDir)) {
-        console.log("creating folder:" + tempDir);
-        fs.mkdirSync(tempDir);
-      }
       // 使用uuid模組生成唯一的檔名，然後將Dicom文件移動到暫存目錄中
       const newFileName = `${uuid.v4()}.dcm`;
       console.log("moving:" + filePath + "->" + newFileName);
@@ -40,6 +34,13 @@ function deleteTempDir() {
 
 async function main() {
 	// 執行程序
+	randomStr = uuid.v4();
+	tempDir = path.join('./temp/', `dicom_${randomStr}`);
+	// 創建暫存目錄，如果不存在的話
+	if (!fs.existsSync(tempDir)) {
+		console.log("creating folder:" + tempDir);
+		fs.mkdirSync(tempDir);
+	}
 	moveDicomFilesToTempDir(dicomDir); // 移動Dicom文件到暫存目錄
 	await storeDicomFilesInTempDir(); // 儲存所有暫存目錄Dicom文件
 	//deleteTempDir(); // 刪除暫存目錄及其中的所有文件
